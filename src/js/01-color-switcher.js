276a1a51a17d1cfff47ebc35a40905ba00802dec
Stanislav Sapunkov <stsapunkov@gmail.com>
2023-04-06 21:03:12 +0300

    nine_start

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,20 @@
+const startButton = document.querySelector("[data-start]");
+const stopButton = document.querySelector("[data-stop]");
+const bodyColorEl = document.querySelector("body");
+
+let timerId = null;
+
+const getRandomHexColor = () => `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+
+startButton.addEventListener("click", () => {
+    timerId = setInterval(() => {
+        bodyColorEl.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+
+    startButton.disabled = true;
+});
+
+stopButton.addEventListener("click", () => {
+    clearInterval(timerId);
+    startButton.disabled = false;
+});
